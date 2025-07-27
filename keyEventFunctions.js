69346769b0adb35cb61f7ae9fc00c76143587828
碳酸氢钠 一氧化二氢溶液 <huz193py@outlook.com>
2025-07-27 18:41:10 +0800

    优化其keydown的事件判断事件

--- keyEventFunctions.js
+++ keyEventFunctions.js
@@ -1,6 +1,8 @@
 function changeInputToInputedString() {
     selectInputRow.innerHTML = newInputString;
     inputLocation = newInputString.length;
+    Cout(newInputString)
+    selectInputRow.innerHTML = newInputString;
 }
 
 window.addEventListener("keydown", function (event) {
@@ -12,43 +14,39 @@ window.addEventListener("keydown", function (event) {
     // inputStringSetsLength = inputStringSets.length;
     // inputRowStringLength --;
 
-    if (involvedKeys.includes(inputKey) == false) {
-        if (specialKeys.includes(inputKey)) {
-            if (inputKey == "ArrowLeft") {
-                if (inputLocation > 0) {
-                    inputLocation --;
-                }
-            } else if (inputKey == "ArrowRight") {
-                if (inputLocation < inputRowStringLength) {
-                    inputLocation ++;
-                }
-            } else if (inputKey == "ArrowUp") {
-                if (selectInputStringSetNum > 1) {
-                    changeInputToInputedString();
-
-                    selectInputStringSetNum --;
-                }
-            } else if (inputKey == "ArrowDown") {
-                if (selectInputStringSetNum < inputStringSetsLength) {
-                    changeInputToInputedString();
-
-                    selectInputStringSetNum ++;
-                }
-            } else if (inputKey == "Backspace") {
-                inputRowBackSpace();
-            } else if (inputKey == "Enter") {
-                slashStop();
-                slashHide();
-
-                searchKeyWords(inputString);
-
-                return;
-            }
-        } else {
-            inputRowAddKey();
+    if (inputKey == "ArrowLeft") {
+        if (inputLocation > 0) {
+            inputLocation--;
         }
-        displayInput();
+    } else if (inputKey == "ArrowRight") {
+        if (inputLocation < inputRowStringLength) {
+            inputLocation ++;
+        }
+    } else if (inputKey == "ArrowUp") {
+        if (selectInputStringSetNum > 1) {
+
+            changeInputToInputedString();
+            selectInputStringSetNum --;
+        }
+    } else if (inputKey == "ArrowDown") {
+        if (selectInputStringSetNum < inputStringSetsLength) {
+
+            changeInputToInputedString();
+            selectInputStringSetNum++;
+        }
+    } else if (inputKey == "Backspace") {
+        inputRowBackSpace();
+    } else if (inputKey == "Enter") {
+        slashStop();
+        slashHide();
+
+        searchKeyWords(inputString);
+
+        return;
+    } else if (inputKey.length == 1) {
+        inputRowAddKey();
     }
+    displayInput();
 });
 
 // 待优化...
