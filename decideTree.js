a127407a0f49404f54150e61b9d260eddabe8090
碳酸氢钠 一氧化二氢溶液 <huz193py@outlook.com>
2025-07-28 20:14:07 +0800

    更正输入无效的输出

--- decideTree.js
+++ decideTree.js
@@ -3,16 +3,15 @@ const author = "NikeMa2011";
 const githubLink = "https://github.com/NikeMa2011/CMD.exe";
 const emailAddress = "huz193py@outlook.com";
 
-const error = "[Error]";
-const warn = "[Warn]";
-
 function run(keyWords) {
     if (keyWords[0] == "ping") {
         ping(keyWords[1]);
     } else if (keyWords[0] == "intro") {
         addParagraph("author: " + author + ", github link: " + githubLink + ", email-address: " + emailAddress);
     } else {
-        addParagraph(error + "invalid input.");
+        addParagraph("\'" + keyWords[0] + "\' is not recognized as an internal or external command,");
+        addParagraph("operable program or batch file.");
+        
         addInputRow();
     }
 }
