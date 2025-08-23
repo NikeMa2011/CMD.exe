afae3208a4bb7d032e75d08825215191edb28380
碳酸氢钠 一氧化二氢溶液 <huz193py@outlook.com>
2025-08-23 18:42:28 +0800

    添加输出中文的功能

--- options.js
+++ options.js
@@ -30,12 +30,12 @@ function request(packNum) {
         }
         setTimeout(() => {
             if (actuallPingValue < 5000) {
-                addParagraph("\tReply form: " + IPAddressString + ": time = " + actuallPingValue + "ms");
+                addParagraph(userLanguage == "zh-CN" ? "\t来自 " + IPAddressString + " 的回复: 时间=" + actuallPingValue + "ms" : "\tReply form: " + IPAddressString + ": time = " + actuallPingValue + "ms");
 
                 pingValueSet[packNum] = actuallPingValue;
                 packRecivedNum ++;
             } else {
-                addParagraph("\tPING: transmit failed. General failure.");
+                addParagraph(userLanguage == "zh-CN" ? "\tPING：传输失败。常见故障。" : "\tPING: transmit failed. General failure.");
 
                 packLosedNum ++;
             }
@@ -50,8 +50,8 @@ function pingFinsh() {
 
     addNullParagraph();
 
-    addParagraph("Ping statistics for " + IPAddressString + ':');
-    addParagraph("\tPackets: Sent = " + packTestNum + ", Recived = " + packRecivedNum + ", Lost = " + packLosedNum + " (" + packLosedNum / packTestNum * 100 + '% loss),');
+    addParagraph(userLanguage == "zh-CN" ? IPAddressString + " 的 Ping 统计信息:" : "Ping statistics for " + IPAddressString + ':');
+    addParagraph(userLanguage == "zh-CN" ? "\t数据包: 已发送 = " + packTestNum + "，已接收 = " + packRecivedNum + "，丢失 = " + packLosedNum + " (" + packLosedNum / packTestNum * 100 + "% 丢失)，" : "\tPackets: Sent = " + packTestNum + ", Recived = " + packRecivedNum + ", Lost = " + packLosedNum + " (" + packLosedNum / packTestNum * 100 + "% loss),");
     
     if (packRecivedNum != 0) {
         for (let i = 0; i < 4; i ++) {
@@ -68,8 +68,8 @@ function pingFinsh() {
         totalPingValue = 0;
         pingValueSet = [];
 
-        addParagraph("Approximate round trip times in milli-seconds:");
-        addParagraph("\tMinimum = " + minimumPingValue + "ms, Maximum = " + maxiumPingValue + "ms, Average = " + averagePingValue + "ms");
+        addParagraph(userLanguage == "zh-CN" ? "往返行程的估计时间(以毫秒为单位):" : "Approximate round trip times in milli-seconds:");
+        addParagraph(userLanguage == "zh-CN" ? "\t最短 = " + minimumPingValue + "ms，最长 = " + maxiumPingValue + "ms，平均 = " + averagePingValue + "ms" : "\tMinimum = " + minimumPingValue + "ms, Maximum = " + maxiumPingValue + "ms, Average = " + averagePingValue + "ms");
         addNullParagraph();
 
         minimumPingValue, maxiumPingValue, averagePingValue = 0;
@@ -82,8 +82,8 @@ function pingFinsh() {
 }
 function ping(IPAddress) {
     if (IPAddress == undefined) {
-        addParagraph("Usage: ping [host]");
-        addParagraph("\tPing the specified host(IPv4 only) until stopped.");
+        addParagraph(userLanguage == "zh-CN" ? "用法: ping [-t]" : "Usage: ping [-t]");
+        addParagraph(userLanguage == "zh-CN" ? "\t-t\tPing 指定的主机，直到停止。" : "\t-t\tPing the specified host until stopped.");
         addNullParagraph();
 
         addInputRow();
@@ -91,18 +91,15 @@ function ping(IPAddress) {
         isIPAddress(IPAddress);
         IPAddressString = IPAddress;
 
+        setRandomPingRange();
+        setRandomPingSwing();
+
+        addParagraph(userLanguage == "zh-CN" ? "正在 " + IPAddressString + " 具有 32 字节的数据:" : "Pinging " + IPAddressString + " with 32 bytes of data:");
+
         if (IPAddressType == publicIP) {
-            setRandomPingRange();
-            setRandomPingSwing();
-
-            addParagraph("Pinging " + IPAddressString + " with 32 bytes of data:");
-
             request(0);
         } else if (IPAddressType == invalidIP || IPAddressType == privateIP) {
             failPing = true;
-
-            addParagraph("Pinging " + IPAddressString + " with 32 bytes of data:");
-
             request(0);
         } else {
             addParagraph("Ping request could not find host " + IPAddress + " . Please check the name and try again.");
@@ -125,7 +122,7 @@ function disclaimer() {
     addParagraph(userLanguage == "zh-CN" ? "输入 \"intro\" 以获得更多信息." : "Type \"intro\" to get more information.");
 }
 function copyRight() {
-    addParagraph(userLanguage == "zh-CN" ? "(c) Microsoft Corporation. 版权所有." : "(c) Microsoft Corporation. All rights reserved.");
+    addParagraph("(c) Microsoft Corporation" + (userLanguage == "zh-CN" ? "。 保留所有权利。" : ". All rights reserved."));
     addNullParagraph();
 
     disclaimer();
@@ -139,10 +136,10 @@ function getVersion() {
 function version() {
     getVersion();
 
-    versionOutputString = "Microsoft Windows";
+    versionOutputString = "Microsoft Windows ";
 
     if (windowsVersion != "Error") {
-        versionOutputString += " [Version " + windowsVersion + "]";
+        versionOutputString += '[' + (userLanguage == "zh-CN" ? "版本" : "Version") + ' ' + windowsVersion + "]";
     }
 
     addParagraph(versionOutputString);
@@ -158,24 +155,24 @@ function version() {
 }
 
 function invalidInput(input) {
-    addParagraph("\'" + input + "\' is not recognized as an internal or external command,");
-    addParagraph("operable program or batch file.");
+    addParagraph("\'" + input + "\'" + (userLanguage == "zh-CN" ? " 不是内部或外部命令，也不是可运行的程序" : " is not recognized as an internal or external command"));
+    addParagraph(userLanguage == "zh-CN" ? "或批处理文件。" : "operable program or batch file.");
     addNullParagraph();
 
     addInputRow(); 
 }
 
 function introduce() {
-    addParagraph("The disclaimer:");
+    addParagraph(userLanguage == "zh-CN" ? "免责声明:" : "The disclaimer:");
     disclaimer();
 
     addNullParagraph();
 
-    addParagraph("This Windows command prompt simulator project is in Github.");
+    addParagraph(userLanguage == "zh-CN" ? "此 Windows 命令提示符模拟器 是一个在 Github 的开源项目。" : "This open-source Windows command prompt simulator project is on Github.");
     addNullParagraph();
-    addParagraph("Author of this project: " + author);
-    addParagraph("Repository URL: \"" + githubLink + "\"");
-    addParagraph("Contact: \"" + emailAddress + "\"");
+    addParagraph((userLanguage == "zh-CN" ? "这个项目的作者: " : "Author of this project: ") + author);
+    addParagraph((userLanguage == "zh-CN" ? "这个项目的 Github 地址URL: \"" : "Github repository URL: \"") + githubLink + "\"");
+    addParagraph((userLanguage == "zh-CN" ? "联系方式: \"" : "Contact: \"") + emailAddress + "\"");
     addNullParagraph();
 
     addInputRow();
