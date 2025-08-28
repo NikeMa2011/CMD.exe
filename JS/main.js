4f1944bf2553d10114fb6ecd18bef1c7aa91564f
碳酸氢钠 一氧化二氢溶液 <huz193py@outlook.com>
2025-08-28 23:24:18 +0800

    删除注释

--- JS/main.js
+++ JS/main.js
@@ -32,8 +32,6 @@ const involvedKeys = ["Escape", "Control", "Alt", "Tab", "Fn", "CapsLock",
                       "Meta", "AudioVolumeMute", "AudioVolumUp", "AudioVlolumeDown",
                       "F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "F10", "F11", "F12"];
 
-// Meta = Windows key ↑
-
 // const allowedKeys = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', "K", 'M', 'L', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 
 //                     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'n', 'l', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 
 //                     0, 1, 2, 3, 4, 5, 6, 7, 8, 9, '/', ':', '<', '>', '?', ',', '.', ';', '[', ']', '{', '}', '-', '=', '_', '+', '!', '@', '#', '$', 
